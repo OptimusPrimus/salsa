0c862005fc03147e3c9a97393ba89c030c8bca47
paul <paul.primus@jku.at>
2024-07-11 19:26:19 +0200

    add description of training

--- scripts/download_checkpoint.sh
+++ scripts/download_checkpoint.sh
@@ -4,6 +4,7 @@ wget https://cloud.cp.jku.at/index.php/s/pMWRbJzXqFPPzgD/download/passt_roberta.
 wget https://cloud.cp.jku.at/index.php/s/pMWRbJzXqFPPzgD/download/passt_roberta.2.ckpt
 wget https://cloud.cp.jku.at/index.php/s/pMWRbJzXqFPPzgD/download/passt_roberta.3.ckpt
 wget https://cloud.cp.jku.at/index.php/s/pMWRbJzXqFPPzgD/download/passt_roberta.4.ckpt
+wget https://cloud.cp.jku.at/index.php/s/pMWRbJzXqFPPzgD/download/passt_roberta.5.ckpt
 
 cat passt_roberta.*.ckpt > passt_roberta.ckpt
 rm passt_roberta.*.ckpt
