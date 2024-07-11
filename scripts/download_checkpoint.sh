cc4c6f45a2a37dd1ae08bf65b147d2cb2da13b6e
paul <paul.primus@jku.at>
2024-07-11 20:10:52 +0200

    add description of training

--- scripts/download_checkpoint.sh
+++ scripts/download_checkpoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-wget https://cloud.cp.jku.at/index.php/s/3kKiQJFk45R7S5k/download/passt_roberta.1.ckpt
+wget https://cloud.cp.jku.at/index.php/s/pMWRbJzXqFPPzgD/download/passt_roberta.1.ckpt
 wget https://cloud.cp.jku.at/index.php/s/aMDLmLTZH4jCHwq/download/passt_roberta.2.ckpt
 wget https://cloud.cp.jku.at/index.php/s/3kKiQJFk45R7S5k/download/passt_roberta.3.ckpt
 wget https://cloud.cp.jku.at/index.php/s/ijLwzPAQrqKRf5P/download/passt_roberta.4.ckpt
@@ -8,8 +8,9 @@ wget https://cloud.cp.jku.at/index.php/s/Np4A5nobAkaSGpY/download/passt_roberta.
 
 cat passt_roberta.*.ckpt > passt_roberta.ckpt
 # rm passt_roberta.*.ckpt
+
+#https://cloud.cp.jku.at/index.php/s/pMWRbJzXqFPPzgD
 #https://cloud.cp.jku.at/index.php/s/aMDLmLTZH4jCHwq
-#https://cloud.cp.jku.at/index.php/s/3kKiQJFk45R7S5k
 #https://cloud.cp.jku.at/index.php/s/3kKiQJFk45R7S5k
 #https://cloud.cp.jku.at/index.php/s/ijLwzPAQrqKRf5P
 #https://cloud.cp.jku.at/index.php/s/Np4A5nobAkaSGpY
